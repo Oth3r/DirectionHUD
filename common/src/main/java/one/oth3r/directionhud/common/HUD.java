7d3e84879b8167f7eaae661a6213f9809bdb3f59
Oth3r <oth3rone@outlook.com>
2024-02-01 16:14:14 -0600

    use Num.inBetween

--- common/src/main/java/one/oth3r/directionhud/common/HUD.java
+++ common/src/main/java/one/oth3r/directionhud/common/HUD.java
@@ -447,16 +447,15 @@ public class HUD {
         if (rotation < 0) {
             rotation += 360.0;
         }
-        //todo helper.inbetween
-        if (0 <= rotation && rotation < 22.5) return "N";
-        else if (22.5 <= rotation && rotation < 67.5) return "NE";
-        else if (67.5 <= rotation && rotation < 112.5) return "E";
-        else if (112.5 <= rotation && rotation < 157.5) return "SE";
-        else if (157.5 <= rotation && rotation < 202.5) return "S";
-        else if (202.5 <= rotation && rotation < 247.5) return "SW";
-        else if (247.5 <= rotation && rotation < 292.5) return "W";
-        else if (292.5 <= rotation && rotation < 337.5) return "NW";
-        else if (337.5 <= rotation && rotation < 360.0) return "N";
+        if (Num.inBetween(rotation,0,22.5)) return "N";
+        else if (Num.inBetween(rotation,22.5,67.5)) return "NE";
+        else if (Num.inBetween(rotation,67.5,112.5)) return "E";
+        else if (Num.inBetween(rotation,112.5,157.5)) return "SE";
+        else if (Num.inBetween(rotation,157.5,202.5)) return "S";
+        else if (Num.inBetween(rotation,202.5,247.5)) return "SW";
+        else if (Num.inBetween(rotation,247.5,292.5)) return "W";
+        else if (Num.inBetween(rotation,292.5,337.5)) return "NW";
+        else if (Num.inBetween(rotation,337.5,360.0)) return "N";
         else return "?";
     }
     public static String getGameTime(boolean t12hr) {
