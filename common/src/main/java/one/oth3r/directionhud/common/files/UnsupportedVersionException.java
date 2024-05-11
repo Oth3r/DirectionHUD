132ef73d0808ac7709960471fd3ef6ba5202900e
Oth3r <oth3rone@outlook.com>
2024-05-11 18:32:08 -0500

    new exception for legacy handling

---
+++ common/src/main/java/one/oth3r/directionhud/common/files/UnsupportedVersionException.java
@@ -0,0 +1,7 @@
+package one.oth3r.directionhud.common.files;
+
+public class UnsupportedVersionException extends Exception {
+    public UnsupportedVersionException() {
+        super("Unsupported Version!");
+    }
+}
