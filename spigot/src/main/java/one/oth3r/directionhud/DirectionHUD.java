fc68f1b278ec8beede3b307100d19e104fe83617
Oth3r <68134921+Oth3r@users.noreply.github.com>
2025-03-24 15:19:41 -0500

    change spigot primary and secondary colors

--- spigot/src/main/java/one/oth3r/directionhud/DirectionHUD.java
+++ spigot/src/main/java/one/oth3r/directionhud/DirectionHUD.java
@@ -24,7 +24,7 @@ public class DirectionHUD extends JavaPlugin implements PluginMessageListener {
     public static Logger LOGGER;
 
 
-    private static final PluginData pluginData = new PluginData(false, "#ff8e16", "#42a0ff");
+    private static final PluginData pluginData = new PluginData(false, "#ef9125", "#ffee35");
 
     public static PluginData getData() {
         return pluginData;
