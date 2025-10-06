12df375d836227e174cc12e013a47b5b70dc4362
Oth3r <68134921+oth3r@users.noreply.github.com>
2025-10-06 11:08:06 -0500

    1.21.9 port

--- fabric/src/main/java/one/oth3r/directionhud/utils/ModEvents.java
+++ fabric/src/main/java/one/oth3r/directionhud/utils/ModEvents.java
@@ -14,6 +14,7 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.WorldSavePath;
 import one.oth3r.directionhud.DirectionHUD;
 import one.oth3r.directionhud.DirectionHUDClient;
@@ -37,11 +38,13 @@ public class ModEvents {
         private static KeyBinding keyBinding;
 
         private static void register() {
+            KeyBinding.Category dhudCategory = KeyBinding.Category.create(Identifier.of(DirectionHUD.MOD_ID, "main"));
+
             keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                     "key.directionhud.keybind.toggle",
                     InputUtil.Type.KEYSYM,
                     GLFW.GLFW_KEY_H,
-                    "category.directionhud.all"
+                    dhudCategory
             ));
 
         }
